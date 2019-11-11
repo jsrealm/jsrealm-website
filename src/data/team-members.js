d4667e30956992277d71ed920fcd7fd65873d8aa
Ameer Jhan <ameerjhanprof@gmail.com>
2019-11-11 19:19:01 +0530

    ref: add jaya krishna

--- src/data/team-members.js
+++ src/data/team-members.js
@@ -1,6 +1,7 @@
 import VilvaAvatar from '../images/vilva-avatar.jpeg';
 import AmeerAvatar from '../images/ameer-avatar.jpg';
 import GaneshAvatar from '../images/ganesh-avatar.jpeg';
+import JayaKumarAvatar from '../images/jaya-kumar-avatar.jpeg';
 
 export const TEAM_MEMBERS = [
   {
@@ -17,5 +18,10 @@ export const TEAM_MEMBERS = [
     name: 'GaneshMani',
     avatar: GaneshAvatar,
     twitter: 'ganeshmani009'
+  },
+  {
+    name: 'Jaya Krishna',
+    avatar: JayaKumarAvatar,
+    twitter: 'askjkrishna'
   },
 ];
