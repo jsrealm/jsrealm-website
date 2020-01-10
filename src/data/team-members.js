993e28636a139832da332bd24c6e2462760a5bd2
Ameer Jhan <ameerjhanprof@gmail.com>
2020-01-10 12:29:27 +0530

    feat: add kiran to team

--- src/data/team-members.js
+++ src/data/team-members.js
@@ -2,6 +2,7 @@ import VilvaAvatar from '../images/vilva-avatar.jpeg';
 import AmeerAvatar from '../images/ameer-avatar.jpg';
 import GaneshAvatar from '../images/ganesh-avatar.jpeg';
 import JayaKrishnaAvatar from '../images/jaya-kumar-avatar.jpeg';
+import KiranAvatar from '../images/kiran-avatar.jpeg';
 
 export const TEAM_MEMBERS = [
   {
@@ -23,5 +24,10 @@ export const TEAM_MEMBERS = [
     name: 'Jaya Krishna Namburu',
     avatar: JayaKrishnaAvatar,
     twitter: 'askjkrishna'
+  },
+  {
+    name: 'Kirankumar Ambati',
+    avatar: KiranAvatar,
+    twitter: _sudo_user
   }
 ];
